33e7f9b10c28317fd5b4eef562d56f579530f434
Artem Evsevev <artem@qrsoft.ru>
2023-12-16 11:26:29 +0300

    tree

--- init.lua
+++ init.lua
@@ -1,28 +1,36 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
     lazypath,
   })
 end
-vim.opt.rtp:prepend(lazypath) 
+vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({
-	"sainnhe/everforest",
-  "williamboman/mason.nvim",
-  "williamboman/mason-lspconfig.nvim",
-  "neovim/nvim-lspconfig",
-  "WhoIsSethDaniel/mason-tool-installer.nvim"
+require('lazy').setup({
+  'sainnhe/everforest',
+  'williamboman/mason.nvim',
+  'williamboman/mason-lspconfig.nvim',
+  'neovim/nvim-lspconfig',
+  'WhoIsSethDaniel/mason-tool-installer.nvim',
+  {
+    'stevearc/conform.nvim',
+    opts = {},
+  },
+  'nvim-tree/nvim-tree.lua',
+  'nvim-tree/nvim-web-devicons',
 })
 
-vim.cmd("colorscheme everforest")
+vim.cmd('colorscheme everforest')
 
 require('base')
 require('keymap')
 require('p-mason')
 require('p-lsp')
 require('lsp-lua')
+require('p-conform')
+require('p-tree')
