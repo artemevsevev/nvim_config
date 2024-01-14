4a21eb9b3d4b95c0a6c03d3454a4772640817f34
Artem Evsevev <artem@qrsoft.ru>
2024-01-14 09:29:48 +0300

    leap

--- init.lua
+++ init.lua
@@ -24,51 +24,7 @@ require('lazy').setup({
   'nvimdev/lspsaga.nvim',
   'nvim-lualine/lualine.nvim',
   {
-    'folke/flash.nvim',
-    event = 'VeryLazy',
-    opts = {},
-    keys = {
-      {
-        's',
-        mode = { 'n', 'x', 'o' },
-        function()
-          require('flash').jump()
-        end,
-        desc = 'Flash',
-      },
-      {
-        'S',
-        mode = { 'n', 'x', 'o' },
-        function()
-          require('flash').treesitter()
-        end,
-        desc = 'Flash Treesitter',
-      },
-      {
-        'r',
-        mode = 'o',
-        function()
-          require('flash').remote()
-        end,
-        desc = 'Remote Flash',
-      },
-      {
-        'R',
-        mode = { 'o', 'x' },
-        function()
-          require('flash').treesitter_search()
-        end,
-        desc = 'Treesitter Search',
-      },
-      {
-        '<c-s>',
-        mode = { 'c' },
-        function()
-          require('flash').toggle()
-        end,
-        desc = 'Toggle Flash Search',
-      },
-    },
+    'ggandor/leap.nvim',
   },
   {
     'folke/todo-comments.nvim',
@@ -179,6 +135,8 @@ require('gruvbox').setup({
   },
 })
 vim.cmd('colorscheme gruvbox')
+
+require('leap').create_default_mappings()
 
 require('base')
 require('keymap')
