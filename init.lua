fd25493f86e34f46bb177cad93d5301fec88e144
Artem Evsevev <artem@qrsoft.ru>
2024-02-04 16:46:44 +0300

    gruvbox-material

--- init.lua
+++ init.lua
@@ -13,7 +13,8 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup({
-  'ellisonleao/gruvbox.nvim',
+  -- 'ellisonleao/gruvbox.nvim',
+  'sainnhe/gruvbox-material',
   {
     'Exafunction/codeium.vim',
     event = 'BufEnter',
@@ -125,23 +126,23 @@ require('lazy').setup({
   },
 })
 
-require('gruvbox').setup({
-  transparent_mode = true,
-  overrides = {
-    GitSignsAdd = { link = 'GruvboxGreenSign' },
-    GitSignsChange = { link = 'GruvboxAquaSign' },
-    GitSignsDelete = { link = 'GruvboxRedSign' },
-    Type = { link = 'GruvboxAqua' },
-    Identifier = { link = 'GruvboxFg1' },
-    Operator = { link = 'GruvboxFg1' },
-    Delimiter = { link = 'GruvboxFg1' },
-    Special = { link = 'GruvboxFg1' },
-    Macro = { link = 'GruvboxBlue' },
-    Function = { link = 'GruvboxYellow' },
-    ['@type.qualifier'] = { link = 'GruvboxRed' },
-  },
-})
-vim.cmd('colorscheme gruvbox')
+-- require('gruvbox').setup({
+--   transparent_mode = true,
+--   overrides = {
+--     GitSignsAdd = { link = 'GruvboxGreenSign' },
+--     GitSignsChange = { link = 'GruvboxAquaSign' },
+--     GitSignsDelete = { link = 'GruvboxRedSign' },
+--     Type = { link = 'GruvboxAqua' },
+--     Identifier = { link = 'GruvboxFg1' },
+--     Operator = { link = 'GruvboxFg1' },
+--     Delimiter = { link = 'GruvboxFg1' },
+--     Special = { link = 'GruvboxFg1' },
+--     Macro = { link = 'GruvboxBlue' },
+--     Function = { link = 'GruvboxYellow' },
+--     ['@type.qualifier'] = { link = 'GruvboxRed' },
+--   },
+-- })
+vim.cmd('colorscheme gruvbox-material')
 
 require('leap').create_default_mappings()
 
