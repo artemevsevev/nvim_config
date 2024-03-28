48532b96f0f541e975f75eec24b8de6bdc41fcdf
Artem Evsevev <artem@qrsoft.ru>
2024-03-28 21:55:58 +0300

    updates

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@ require('lazy').setup({
     'sainnhe/gruvbox-material',
     config = function()
       vim.g.gruvbox_material_foreground = 'original'
-      vim.g.gruvbox_material_background = 'hard'
+      -- vim.g.gruvbox_material_background = 'original'
       vim.g.gruvbox_material_statusline_style = 'original'
     end,
   },
