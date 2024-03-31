8cd0e20219f0e69f74b54f805c4ce44126babefa
Artem Evsevev <artem@qrsoft.ru>
2024-03-31 06:46:58 +0300

    format

--- lua/p-conform.lua
+++ lua/p-conform.lua
@@ -1,15 +1,17 @@
 require('conform').setup({
-  formatters_by_ft = {
-    lua = { 'stylua' },
-    json = { 'prettierd' },
-    yaml = { 'prettierd' },
-    html = { 'prettierd' },
-    css = { 'prettierd' },
-    markdown = { 'prettierd' },
-    graphql = { 'prettierd' },
-  },
-  format_on_save = {
-    timeout_ms = 500,
-    lsp_fallback = true,
-  },
+    formatters_by_ft = {
+        lua = { 'stylua' },
+        json = { 'prettierd' },
+        yaml = { 'prettierd' },
+        html = { 'prettierd' },
+        css = { 'prettierd' },
+        markdown = { 'prettierd' },
+        graphql = { 'prettierd' },
+        go = { 'goimports', 'gofmt' },
+        rust = { 'rustfmt' },
+    },
+    format_on_save = {
+        timeout_ms = 500,
+        lsp_fallback = true,
+    },
 })
