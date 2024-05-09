831c8df90d04eec47a680ae44a33e5e781bc179b
Artem Evsevev <artem@qrsoft.ru>
2024-05-09 17:45:27 +0300

    tmux

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -38,30 +38,30 @@ keymap.set(
 keymap.set('v', '<c-j>', [[:m '>+1<cr>gv=gv]])
 keymap.set('v', '<c-k>', [[:m '<-2<cr>gv=gv]])
 
-keymap.set(
-  'n',
-  '<C-h>',
-  '<C-w><C-h>',
-  { desc = 'Move focus to the left window' }
-)
-keymap.set(
-  'n',
-  '<C-l>',
-  '<C-w><C-l>',
-  { desc = 'Move focus to the right window' }
-)
-keymap.set(
-  'n',
-  '<C-j>',
-  '<C-w><C-j>',
-  { desc = 'Move focus to the lower window' }
-)
-keymap.set(
-  'n',
-  '<C-k>',
-  '<C-w><C-k>',
-  { desc = 'Move focus to the upper window' }
-)
+-- keymap.set(
+--   'n',
+--   '<C-h>',
+--   ':wincmd h<CR>',
+--   { desc = 'Move focus to the left window' }
+-- )
+-- keymap.set(
+--   'n',
+--   '<C-l>',
+--   ':wincmd l<CR>',
+--   { desc = 'Move focus to the right window' }
+-- )
+-- keymap.set(
+--   'n',
+--   '<C-j>',
+--   ':wincmd j<CR>',
+--   { desc = 'Move focus to the lower window' }
+-- )
+-- keymap.set(
+--   'n',
+--   '<C-k>',
+--   ':wincmd k<CR>',
+--   { desc = 'Move focus to the upper window' }
+-- )
 
 keymap.set('n', 'n', 'nzzzv')
 keymap.set('n', 'N', 'Nzzzv')
