a387a5874609a6ee2a52f25385ac1c4ac6252d94
Artem Evsevev <artem@qrsoft.ru>
2024-05-15 19:50:34 +0300

    codeium

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -72,31 +72,30 @@ return {
           behavior = cmp.ConfirmBehavior.Replace,
           select = true,
         }),
-        ['<Tab>'] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-          elseif luasnip.expand_or_locally_jumpable() then
-            luasnip.expand_or_jump()
-          else
-            fallback()
-          end
-        end, { 'i', 's' }),
-        ['<S-Tab>'] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif luasnip.locally_jumpable(-1) then
-            luasnip.jump(-1)
-          else
-            fallback()
-          end
-        end, { 'i', 's' }),
+        -- ['<Tab>'] = cmp.mapping(function(fallback)
+        --   if cmp.visible() then
+        --     cmp.select_next_item()
+        --   elseif luasnip.expand_or_locally_jumpable() then
+        --     luasnip.expand_or_jump()
+        --   else
+        --     fallback()
+        --   end
+        -- end, { 'i', 's' }),
+        -- ['<S-Tab>'] = cmp.mapping(function(fallback)
+        --   if cmp.visible() then
+        --     cmp.select_prev_item()
+        --   elseif luasnip.locally_jumpable(-1) then
+        --     luasnip.jump(-1)
+        --   else
+        --     fallback()
+        --   end
+        -- end, { 'i', 's' }),
       }),
       window = {
         -- completion = cmp.config.window.bordered(),
         -- documentation = cmp.config.window.bordered(),
       },
       sources = {
-        { name = 'codeium' },
         { name = 'nvim_lsp' },
         { name = 'nvim_lua' },
         { name = 'luasnip' },
@@ -125,12 +124,7 @@ return {
             return vim_item
           else
             -- From lspkind
-            return lspkind.cmp_format({
-              mode = 'symbol',
-              maxwidth = 50,
-              ellipsis_char = '...',
-              symbol_map = { Codeium = '' },
-            })(entry, vim_item)
+            return lspkind.cmp_format()(entry, vim_item)
           end
         end,
       },
