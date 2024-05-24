cf43cdf333e49b1d757c7f12c1e146b3d5cb9285
Artem Evsevev <artem@qrsoft.ru>
2024-05-24 21:39:27 +0300

    indent

---
+++ lua/plugins/indent.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    'lukas-reineke/indent-blankline.nvim',
+    opts = {},
+    config = function()
+      require('ibl').setup({
+        indent = { char = '│' },
+        scope = {
+          show_start = false,
+          show_end = false,
+        },
+      })
+    end,
+  },
+}
