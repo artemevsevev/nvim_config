5e1910bed74da9b42490ed4378d80c5cdc19e041
Artem Evsevev <artem@qrsoft.ru>
2024-06-08 07:01:50 +0300

    updates

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -29,11 +29,6 @@ return {
         ':Telescope find_files <CR>'
       ),
       dashboard.button(
-        'n',
-        ' ' .. ' New file',
-        ':ene <BAR> startinsert <CR>'
-      ),
-      dashboard.button(
         'r',
         ' ' .. ' Recent files',
         ':Telescope oldfiles <CR>'
@@ -48,7 +43,7 @@ return {
       --   ' ' .. 'Restore Session',
       --   '<cmd>lua require("persistence").load()<cr>'
       -- ),
-      dashboard.button('c', ' ' .. ' Config', ':e $MYVIMRC <CR>'),
+      -- dashboard.button('c', ' ' .. ' Config', ':e $MYVIMRC <CR>'),
       dashboard.button('l', '󰒲 ' .. ' Lazy', ':Lazy<CR>'),
       dashboard.button('m', '󱌣 ' .. ' Mason', ':Mason<CR>'),
       dashboard.button('q', ' ' .. ' Quit', ':qa<CR>'),
